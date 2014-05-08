06224bfc21ed89ef2f5ec246d88133d78fc53b65
Homère <hfaivresaito@gmail.com>
2014-05-08 15:59:12 +0800

    hostname addition

--- scripts/avahi/create-service.py
+++ scripts/avahi/create-service.py
@@ -11,7 +11,8 @@ import sys
 
 filename = str(sys.argv[1])+'.service' # file name is the first cmd line argument
 path = '/etc/avahi/services/'+filename # create path to the /etc/avahi/services/ folder
-
+os.system('sudo cat /etc/hostname')
+hostname = sys.stdin
 os.system('sudo touch '+path) # create file with root privileges
 
 target = open(path, 'w')
@@ -23,7 +24,7 @@ line4 = '  <name replace-wildcards="yes">'+str(sys.argv[1])+'</name>'
 line5 = '  <service>'
 line6 = '    <type>_'+str(sys.argv[1])+'._tcp</type>'
 line7 = '    <port>'+sys.argv[2]+'</port>' # port number is the second cmd line argument
-line8 = '    <txt-record>path=/data/shared/Music</txt-record>' # This line should be changed...not realy sure of what to put here.
+# line8 = '    <txt-record>path=/data/shared/Music</txt-record>' # This line should be changed...not realy sure of what to put here.
 line9 = '  </service>'
 line10 = '</service-group>'
 
@@ -42,8 +43,8 @@ target.write(line6)
 target.write("\n")
 target.write(line7)
 target.write("\n")
-target.write(line8)
-target.write("\n")
+#target.write(line8)
+#target.write("\n")
 target.write(line9)
 target.write("\n")
 target.write(line10)
