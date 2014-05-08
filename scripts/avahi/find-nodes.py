7e9a45428c30c32d9871044ad3eb95dafe2608d9
Clément Garnier <garnierclement@yahoo.fr>
2014-05-08 11:02:53 +0000

    find-nodes.phy

--- scripts/avahi/find-nodes.py
+++ scripts/avahi/find-nodes.py
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/usr/bin/python
 
-type=$(cat ../../definitions/avahi-node-service.txt)
+# import os
+# import commands
 
-echo $type
+# service_type = commands.getoutput("cat ../../definitions/node-services.txt") 
+# out = commands.getoutput("avahi-browse -rt ")
+
+# print(service_type)
+
+import subprocess
+
+out = subprocess.check_output("avahi-browse -rt "+"_node._tcp"+" | grep hostname", shell=True)
+print(out)
