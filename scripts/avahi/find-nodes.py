436dc0a03267ee10c64144b97c99964a0c7c68dd
Homère <hfaivresaito@gmail.com>
2014-05-08 19:52:35 +0800

    find-node test v3

--- scripts/avahi/find-nodes.py
+++ scripts/avahi/find-nodes.py
@@ -10,18 +10,25 @@ import os
 
 import subprocess
 
-out = subprocess.check_output("avahi-browse -rt "+"_node._tcp"+" | grep hostname", shell=True)
+hostname = subprocess.check_output("avahi-browse -rt "+"_node._tcp"+" | grep hostname", shell=True)
+hostname_array = hostname.split("\n")
 
-filename = 'test'
-target = open(filename, 'w')
-target.write(out)
+address = subprocess.check_output("avahi-browse -rt "+"_node._tcp"+" | grep address", shell=True)
+address_array = address.split("\n")
+
+port = subprocess.check_output("avahi-browse -rt "+"_node._tcp"+" | grep port", shell=True)
+port_array = port.split("\n")
+
+info = subprocess.check_output("avahi-browse -rt "+"_node._tcp"+" | grep txt", shell=True)
+info_array = info.split("\n")
+
+
+target = open('node_list.csv','w')
+
+
+for i in range(len(hostname_array)):
+	content = hostname_array[i]+';'+address_array[i]+';'+port_array[i]+';'+info_array[i]+';'+"\n"
+	target.write(content)
+
 target.close()
-
-hostname_array = []
-with open("test", "r") as f:
-  for line in f:
-    hostname_array.append(line)
-
-print hostname_array[1]
-print hostname_array[0]
 
