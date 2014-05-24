676596dee61c69c78f33431652dd73a9b97f42f9
Clément Garnier <garnierclement@yahoo.fr>
2014-05-24 13:05:21 +0800

    adding comments and dev files for mDNS modules in Node.js

--- scripts/bootstrap/pi/install-tools.sh
+++ scripts/bootstrap/pi/install-tools.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
 sudo apt-get update
+
+# Avahi daemon and tools
 sudo apt-get install avahi-daemon
 sudo apt-get install avahi-utils
 sudo apt-get install avahi-discover
+
+# for local ARP scan on the network
 sudo apt-get install arp-scan
+
+# Node.js
 sudo apt-get install nodejs
+
+# for mDNS module in Node.js 
+sudo apt-get install libavahi-compat-libdnssd-dev
