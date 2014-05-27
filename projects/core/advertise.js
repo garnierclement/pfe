428d1d591566fa10f55ea70fc3413d2cddb6f12a
Clément Garnier <garnierclement@yahoo.fr>
2014-05-27 23:34:11 +0800

    exception handler for mdns advertisement

---
+++ projects/core/advertise.js
@@ -0,0 +1,18 @@
+function createAdvertisement()  {
+    try {
+        var mdns_txt_record = {
+            name: domoConfig.domoConf.svcName,
+            port: domoConfig.domoConf.svcPort
+        };
+        var advert = mdns.createAdvertisement(mdns.tcp(domoConfig.domoConf.svcName) , domoConfig.domoConf.svcPort, {txtRecord: mdns_txt_record});
+        advert.on('error', function(error) {
+            console.log("advert ERROR ", error);
+            setTimeout(createAdvertisement, 30 * 1000);
+        });
+        advert.start();
+    } catch (ex){
+        console.log("advert creation ERROR ");
+        setTimeout(createAdvertisement, 30 * 1000);
+    } 
+} 
+}
