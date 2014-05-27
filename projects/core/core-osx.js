03c7d9a1307c505acba4eeb1fbe81b696152ae85
Homère <hfaivresaito@gmail.com>
2014-05-27 16:31:25 +0800

    service browsing for inch

--- projects/core/core-osx.js
+++ projects/core/core-osx.js
@@ -19,7 +19,35 @@ var inch = mdns.createAdvertisement(mdns.tcp('http'), 45678, {txtRecord: txt_rec
 
 inch.start();
 
+// _node._tcp. service browser
+
+var browser = mdns.createBrowser(mdns.tcp('airdrop'));
+var network_cap = new network_capabilities();
+
+browser.on('serviceUp', function(service) {
+  console.log("service up: ", service.name);
+  network_cap.nodes.push(new node(service.host, service.addresses));
+  console.log(network_cap.nodes);
+});
+browser.on('serviceDown', function(service) {
+  console.log("service down: ", service.name);
+});
+
+browser.start();
 
 // function zeroconfAdvertisement() {}
+
+
+// Creation of new node object 
+
+function network_capabilities (){
+	this.nodes = [];
+};
+
+function node (host, ip){
+	this.host = host;
+	this.ip = ip;
+};
+
 
 
