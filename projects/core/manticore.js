4454fc60c01e3e485fba1fffac561fa2a865af2e
Clément Garnier <garnierclement@yahoo.fr>
2014-05-29 15:33:26 +0800

    debug self.nodes

--- projects/core/manticore.js
+++ projects/core/manticore.js
@@ -106,10 +106,9 @@ function Core()
 	this.browser.on('serviceUp', function(service) {
 		console.log('[INCH] Service up: '+service.name+' at '+service.addresses+' ('+service.networkInterface+')');
 		if(!findIdNodes(self.nodes,service.txtRecord.id)){
-			console.log(service.txtRecord.id);
-			console.log(findIdNodes(self.nodes,service.txtRecord.id));
 			self.nodes.push(new Node(service.host, service.name, service.addresses, self.uuid, service.txtRecord.id));
 		}
+		console.log(self.nodes);
 	});
 	this.browser.on('serviceDown', function(service) {
 		deleteDeadNode(self.nodes,service.name);
