4f6e8818c01fd3fdb34be2000e94d35ff7620360
Clément Garnier <garnierclement@yahoo.fr>
2014-06-04 15:02:30 +0800

    core: main app with http api

--- projects/core/app.js
+++ projects/core/app.js
@@ -1,5 +1,47 @@
-// main entry point ?
+#!/usr/bin/env node
 
 var core = require('./manticore.js');
 
-console.log(core.name());
+var express = require('express');
+
+var loch_api = express();
+
+loch_api.listen(3000, function() {
+	console.log('Listening');
+});
+
+// gracefully exit on ctrl+C (SIGINT)
+process.on('SIGINT', function() {
+  //subscriber.close();
+  core.close();
+});
+
+
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function() {
+	var chunk = process.stdin.read();
+	if (chunk !== null) {
+		if (chunk == "debug\n") {
+			console.log(core.nodes);
+		}
+		else {
+			core.publisher.send(chunk);
+			console.log("[INCH] Published: "+chunk);
+		}
+	}
+});
+core.on('ready', function() {
+
+	loch_api.get('/', function(req, res) {
+		res.set({'Content-Type': 'application/json'});
+		res.send({nodes: core.nodes });
+	});
+});
+
+// register callback on events before init
+core.init();
+
+
+
