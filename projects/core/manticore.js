e69b8510f34aae486e187d157ec444fac7b4b29e
Clément Garnier <garnierclement@yahoo.fr>
2014-06-09 17:46:53 +0800

    core: refactoring using Core.prototype

--- projects/core/manticore.js
+++ projects/core/manticore.js
@@ -1,7 +1,7 @@
 /**
  * Constants
  */
-const NODE_SERVICE = 'node';
+const NODE_SERVICE = 'node'; // _node._tcp
 const INCH_PORT = 32323;
 const LOCAL_PORT = 42424;
 const MACH_PORT = 45454;
@@ -15,7 +15,7 @@ var zmq = require('zmq');		// ZeroMQ
 var dgram = require('dgram');	// for UDP sockets
 var util = require('util'),		// extend the Core to be an EventEmitter
 	EventEmitter = require('events').EventEmitter;
-var Node = require('./node.js');
+var Node = require('./node.js');// Node object
 
 /**
  * Core object
@@ -33,102 +33,111 @@ function Core()
 	// _node._tcp. service browser
 	this.browser = mdns.createBrowser(mdns.tcp(NODE_SERVICE));
 	this.mach = zmq.socket('rep');
-
-	// register browser events
-	this.browser.on('serviceUp', function(service) {
-		console.log('+[INCH] Service up: '+service.name+' at '+service.addresses+' ('+service.networkInterface+')');
-		if(!findIdNodes(self.nodes,service.txtRecord.id)){
-			self.nodes.push(new Node(service.host, service.name, service.addresses, self.uuid, service.txtRecord.id));
-			console.log('+[INCH] Adding node id '+service.txtRecord.id);
-		}
-		else {
-			console.log('![INCH] Node id '+service.txtRecord.id+' is already present');
-		}
-	});
-	this.browser.on('serviceDown', function(service) {
-		console.log('-[INCH] Service down: '+service.name+' ('+service.networkInterface+')');
-		deleteDeadNode(self.nodes,service.name);
-
-	});
-	this.browser.on('error', function(error) {
-		console.log('![INCH] Browser error: '+error)
-	});
-
-	// initialisation
-	this.init = function() {
-		// start init
-		console.log('+[CORE] Core starting on '+self.name);
-		// bind local socket
-		self.loch.bind(LOCAL_PORT, '127.0.0.1', function() {
-			var address = self.loch.address();
-			console.log('+[LOCH] Local UDP socket listening on '+address.address+':'+address.port);
-		});
-		// TODO for all theses initialization
-		// see if async and waterfall can simplify the code (callback hell ?)
-		self.publisher.bind('tcp://*:'+INCH_PORT, function(err) {
-			if (err) {
-				console.log('![INCH] Publisher binding error: '+err);
-			}
-			else {
-				// bind ok
-				console.log('+[INCH] Publisher socket listening on '+INCH_PORT);
-				// start advertising
-				self.advertiser.start();
-				console.log('+[CORE] Advertising _'+NODE_SERVICE+'._tcp on '+INCH_PORT);
-				// start browser
-				self.browser.start();
-				console.log('+[INCH] Start browsing for _'+NODE_SERVICE+'._tcp services');
-
-			}
-		});
-		self.mach.bind('tcp://*:'+MACH_PORT, function(err) {
-			if (err) {
-				console.log('![MACH] Socket binding error: '+err);
-			}
-			else {
-				console.log('+[MACH] Publisher socket listening on '+MACH_PORT);
-			}
-		});
-		self.emit('ready');
-	};
-
-	// toString()
-	this.toString = function() 
-	{
-		return this.name;
-	};
-
-	// closing, cleaning
-	this.close = function() {
-		self.browser.stop();
-		self.advertiser.stop();
-		console.log('[CORE] Closing');
-		process.exit();
-	};
 }
+// Inherit from `EventEmitter.prototype`.
 util.inherits(Core, EventEmitter);
 
-/**
- * Check if running on Mac OS X
- * @return {Boolean} true if OS X
- */
-function isDarwin() {
-	if (require('os').platform() == 'darwin') return true;
-	else return false;
-}
+// Expose the Core singleton 
+// Provide self for convenience within module
+var self = module.exports = new Core();
 
 /**
- * Check if running on Linux
- * @return {Boolean} true if Linux
+ * Initiliaze the Core singleton
+ * Binding sockets, advertising and browsing for other _node._tcp cores
+ * Emit 'ready' event when initialized
  */
-function isLinux() {
-	if (require('os').platform() == 'linux') return true;
-	else return false;
-}
+Core.prototype.init = function() {
+	console.log('+[CORE] Core starting on '+this.name);
+	// bind local socket
+	this.loch.bind(LOCAL_PORT, '127.0.0.1', function() {
+		var address = self.loch.address();
+		console.log('+[LOCH] Local UDP socket listening on '+address.address+':'+address.port);
+	});
+	// bind publish socket and start advertising and browsing
+	this.publisher.bind('tcp://*:'+INCH_PORT, function(err) {
+		if (err) {
+			console.log('![INCH] Publisher binding error: '+err);
+		}
+		else {
+			console.log('+[INCH] Publisher socket listening on '+INCH_PORT);
+			self.advertise();
+			self.browse();
+		}
+	});
+	// bind mach channel
+	self.mach.bind('tcp://*:'+MACH_PORT, function(err) {
+		if (err)
+			console.log('![MACH] Socket binding error: '+err);
+		else
+			console.log('+[MACH] Publisher socket listening on '+MACH_PORT);
+	});
+	self.emit('ready');
+};
+
+/**
+ * Start mDNS advertisement of _node._tcp service
+ */
+Core.prototype.advertise = function() {
+	this.advertiser.start();
+	console.log('+[CORE] Advertising _'+NODE_SERVICE+'._tcp on '+INCH_PORT);
+};
+
+Core.prototype.browse = function() {
+	this.browser.start();
+	console.log('+[INCH] Start browsing for _'+NODE_SERVICE+'._tcp services');
+};
+
+/**
+ * Handle the discovery of new _node._tcp service
+ * (mDNS browser event)
+ */
+self.browser.on('serviceUp', function(service) {
+	console.log('+[INCH] Service up: '+service.name+' at '+service.addresses+' ('+service.networkInterface+')');
+
+	if(!findNodeById(self.nodes,service.txtRecord.id)){
+		self.nodes.push(new Node(service.host, service.name, service.addresses, self.uuid, service.txtRecord.id));
+		console.log('+[INCH] Adding node id '+service.txtRecord.id);
+	}
+	else {
+		console.log('![INCH] Node id '+service.txtRecord.id+' is already present');
+	}
+});
+
+/**
+ * Delete dead node from local node list
+ */
+self.browser.on('serviceDown', function(service) {
+	console.log('-[INCH] Service down: '+service.name+' ('+service.networkInterface+')');
+	deleteDeadNode(self.nodes,service.name);
+});
+
+/**
+ * Handle mDNS browser errors
+ */
+self.browser.on('error', function(error) {
+	console.log('![INCH] Browser error: '+error);
+});
+
+/**
+ * Get the name of the Core
+ */
+Core.prototype.toString = function() {
+	return this.name;
+};
+
+/**
+ * Stopping mDNS advertising/browsing and exiting
+ */
+Core.prototype.close = function() {
+	console.log('[CORE] Closing');
+	this.browser.stop();
+	this.advertiser.stop();
+	process.exit();
+};
 
 /**
  * Delete node when service down
- * @param  {Node[]}	nodes      list of nodes
+ * @param  {Node[]}	nodes      list of Node objects
  * @param  {String} node_name  node canonical name to be deleted
  */
 function deleteDeadNode(nodes, node_name){
@@ -141,19 +150,19 @@ function deleteDeadNode(nodes, node_name){
 		console.log('-[INCH] Deleting node '+node_name);
 	}
 	else {
-		console.log('![INCH] Error cannot delete node '+node_name+', not found');
+		console.log('![INCH] Error cannot delete node '+node_name+', not found ; no harm, maybe just a duplicate serviceDown');
 	}
 }
 
 /**
  * Check if a particular node is present within a list of node
- * Using uuid to distinguish nodes
+ * Using UUID to disambiguate nodes
  * 
  * @param  {Node[]} nodes list of nodes
  * @param  {String} uuid  uuid of the sought node
  * @return {Boolean}      return true if found
  */
-function findIdNodes(nodes, uuid){
+function findNodeById(nodes, uuid){
 	var res = false;
 	for(k in nodes){
 		if (nodes[k].id == uuid)  
@@ -182,7 +191,20 @@ function createAdvertisement(uuid)  {
     return advertiser;
 }
 
-/*
- * Expose the Core object
+/**
+ * Check if running on Mac OS X
+ * @return {Boolean} true if OS X
  */
-module.exports = new Core();
+function isDarwin() {
+	if (require('os').platform() == 'darwin') return true;
+	else return false;
+}
+
+/**
+ * Check if running on Linux
+ * @return {Boolean} true if Linux
+ */
+function isLinux() {
+	if (require('os').platform() == 'linux') return true;
+	else return false;
+}
