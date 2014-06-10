f05946e5e0cd155b03d0e6a24e30a3a3d28cc3fb
Clément Garnier <garnierclement@yahoo.fr>
2014-06-10 15:40:53 +0800

    generate and execute skeleton

--- projects/core/trigger.js
+++ projects/core/trigger.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /** 
  * Module dependencies
  */
@@ -7,3 +9,11 @@ var child;
 child = exec('ls -la', function(error, stdout, stderr) {
 	console.log(stdout);
 });
+
+function generate() {
+
+}
+
+function execute(cmd) {
+
+}
