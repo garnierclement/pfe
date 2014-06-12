187f12fc464158fb4c46d60d4f2071da51436bd7
Clément Garnier <garnierclement@yahoo.fr>
2014-06-12 17:08:10 +0800

    add variable for generation and return pid for child

--- projects/manticore/trigger.js
+++ projects/manticore/trigger.js
@@ -5,10 +5,11 @@ var exec = require('child_process').exec;
 
 exports.execute = function(cmd, callback) {
 	try {
-		exec(cmd, function(err, stdout, stderr){
+		var child = exec(cmd, function(err, stdout, stderr){
 			console.log("+[EXEC] Executing "+cmd+"\n"+stdout+stderr);
 			callback(stdout, stderr);
 		});
+		return child.pid;
 	}
 	catch(e) {
 		console.log("![EXEC] "+e);
@@ -16,7 +17,9 @@ exports.execute = function(cmd, callback) {
 };
 
 exports.generate = function(ip, port, file, output) {
-	var sed = "sed -e '/^#X msg [0-9]* [0-9]* connect/s/connect [0-9a-z_.\-]* [0-9]*/connect "+ip+" "+port+"/' "+file+" > "+ output;
+	var outputDir = "../../var/run/";
+	var inputDir = "../../pd/";
+	var sed = "sed -e '/^#X msg [0-9]* [0-9]* connect/s/connect [0-9a-z_.\-]* [0-9]*/connect "+ip+" "+port+"/' "+inputDir+file+" > "+ outputDir+output;
 	console.log(sed);
 	exec(sed, function(err,stdout, stderr) {
 		console.log('+[GEN] File generated '+output);
