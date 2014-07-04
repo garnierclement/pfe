d1fbc99c047800ae20597864fdc81c7c13bfebbb
Homère <hfaivresaito@gmail.com>
2014-07-04 17:53:31 +0800

    generateMousePosition.sh functional

--- sensors/mouse/generateMousePosition.sh
+++ sensors/mouse/generateMousePosition.sh
@@ -5,9 +5,8 @@ ip_dst=$1
 # $2 is port of the endpoint
 port_dst=$2
 # $3 is the name of the generated pure data file
-output="../var/run/"$3
+output="../../var/run/"$3
 
 # Copy the mousePosition.pd file and change the 'connect' endpoint
-#sed -e '/^#X msg [0-9]* [0-9]* connect/s/connect [0-9a-z_.\-]* [0-9]*/connect "+ip+" "+port+"/' "+inputDir+file+" > "+ outputDir+output;
+sed -e '/^#X msg [0-9]* [0-9]* connect/s/connect [0-9a-z_.\-]* [0-9]*/connect '$ip_dst'\ '$port_dst'/' mousePosition.pd > $output;
 
-echo '/^#X msg [0-9]* [0-9]* connect/s/connect [0-9a-z_.\-]* [0-9]*/connect '$ip_dst' '$ip_port' '
