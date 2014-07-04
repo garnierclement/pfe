f36b83915fe991d5361c1e3663b6e9ddaea4b2a5
Homère <hfaivresaito@gmail.com>
2014-07-04 18:38:33 +0800

    checkMouse-osx.sh functional

--- sensors/mouse/checkMouse-osx.sh
+++ sensors/mouse/checkMouse-osx.sh
@@ -3,3 +3,32 @@
 # use system_profiler | grep Trackpad or mouse
 # system_profiler SPUSBDataType
 # check for HI Device inputs
+
+failure=0
+success=1
+
+echo "Checking for mouse or trackpad..."
+
+if system_profiler SPUSBDataType | grep MOUSE 
+then 
+	echo "MOUSE detected!"
+	exit $success 
+
+elif system_profiler SPUSBDataType | grep Trackpad 
+then 
+	echo "Trackpad detected!"
+	exit $success 
+
+elif system_profiler SPUSBDataType | grep Mouse 
+then 
+	echo "Mouse detected!"
+	exit $success
+
+elif system_profiler SPUSBDataType | grep mouse
+then
+	echo "mouse detected!" 
+	exit $success
+else
+	exit $failure
+fi
+
