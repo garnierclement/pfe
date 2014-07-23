a1047bfb746ea04c53e4dc99c2e39fe14573caab
Clément Garnier <garnierclement@yahoo.fr>
2014-07-23 14:46:41 +0800

    remove useless code

--- projects/manticore/record.js
+++ projects/manticore/record.js
@@ -18,20 +18,3 @@ module.exports = Record;
 Record.prototype.addChild = function(child) {
 	this.child = child;
 };
-
-
-///// unfinished
-exports.findResourceIn = function(list, type, res, callback) {
-	for (var idx = 0; idx < list.length; idx++) {
-		if (list[idx].resource === res) {
-			switch(list[idx].type) {
-				case 'client_request':
-					break;
-				case 'active_resource':
-					break;
-				default:
-					break;
-			}
-		}
-	}
-};
