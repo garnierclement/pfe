28407efb66b9c5a0869d224b67acbd54d310ffd7
Clément Garnier <garnierclement@yahoo.fr>
2014-07-25 19:32:45 +0800

    remove doctoc in PDF documentation

--- docs/doc.sh
+++ docs/doc.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# Automatize the generation of PDF and HTML documentation compiling the READMEs
+
+# Create temp files to remove ToC from DocToc for PDF output
+sed '/<!-- START/,/<!-- END/d' ../sensors/README.md > tmp_sensor.md
+sed '/<!-- START/,/<!-- END/d' ../projects/manticore/README.md > tmp_manticore.md
+
 # Use Pandoc to generate the PDF documentation
-pandoc ../sensors/README.md ../projects/manticore/README.md \
+pandoc tmp_sensor.md tmp_manticore.md \
 	-o doc.pdf \
 	--tab-stop=2 --toc --toc-depth=4 -N --chapters \
 	-V title="\textbf{Manticore} \\\ Documentation \\\ and \\\ Programming Guide" \
@@ -10,6 +16,10 @@ pandoc ../sensors/README.md ../projects/manticore/README.md \
 	-V geometry="margin=1.2in" \
 	-V date="\today" \
 	-V papersize="a4paper"
+
+# Remove temp files
+rm tmp_sensor.md
+rm tmp_manticore.md
 
 # Use Pandoc to generate the HTML documentation
 pandoc ../sensors/README.md ../projects/manticore/README.md \
