ad9a0eb07a151e3bacfea7812ac8ff564547d71a
Clément Garnier <garnierclement@yahoo.fr>
2014-07-28 19:42:18 +0800

    Moving from HTML to eft for installer resources

--- scripts/osx-package/resources/md2html.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-pandoc -s welcome.md -o welcome.html
-pandoc -s conclusion.md -o conclusion.html
---
+++ scripts/osx-package/resources/md2rtf.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pandoc -s welcome.md -o welcome.rtf
+pandoc -s conclusion.md -o conclusion.rtf
